7773c05b40d0704af3dc13805cf9cbf0d26879e2
xCrixus <maxi.ruiz.1991@gmail.com>
2019-08-30 00:50:01 -0300

    CH-02 - Added firefighter strategies | work in progress

--- Metodologias1.Heroes/Heroes/Heroes/Firefighter.cs
+++ Metodologias1.Heroes/Heroes/Heroes/Firefighter.cs
@@ -1,17 +1,29 @@
 ﻿using System;
+using Heroes.FiremanStrategies;
+using Heroes.Interfaces;
+using Heroes.Places;
 
 namespace Heroes.Heroes
 {
     public class Firefighter
     {
-        public void PutOutFire()
+        public IExtinguishFire ExtinguishStrategy { get; set; } = new Sequential();
+
+        public void PutOutFire(IPlace place, Street street)
         {
             Console.WriteLine("The fireman is putting out the fire.");
+            ExtinguishStrategy.ExtinguishFire(place.GetFields(), street.WaterFlowPerMinute);
+            Console.WriteLine($"The fire of the {place.ToString()} went out!!!");
         }
 
         public void GetCatOutOfTree()
         {
             Console.WriteLine("The fireman is helping the cat down.");
+        }
+
+        public void ChangeExtinguishStrategy(IExtinguishFire strategy)
+        {
+            this.ExtinguishStrategy = strategy;
         }
     }
 }
